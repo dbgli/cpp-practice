6105238b7b98b723ba23c16180f8f060d0c7fc30
DebuggerLi <DebuggerLi.dev@outlook.com>
2018-02-26 22:03:58 +0800

    add.6_1

---
+++ cpp_primer/chapter_6/6_1/6_1/main.cpp
@@ -0,0 +1,50 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int fact(int val) {
+//    int ret = 1;
+//    while (val > 1)
+//        ret *= val--;
+//    return ret;
+//}
+//
+//int main() {
+//    cout << "5! = " << fact(5) << endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int fact(int val) {
+//    int ret = 1;
+//    while (val > 1)
+//        ret *= val--;
+//    return ret;
+//}
+//
+//int main() {
+//    int i;
+//    cin >> i;
+//    cout << i << "! = " << fact(i) << endl;
+//    return 0;
+//}
+
+#include <iostream>
+
+using namespace std;
+
+double absolute(double val) {
+    if (val < 0)
+        val = -val;
+    return val;
+}
+
+int main() {
+    double i;
+    cin >> i;
+    cout << "abs(i) = " << absolute(i) << endl;
+    return 0;
+}
